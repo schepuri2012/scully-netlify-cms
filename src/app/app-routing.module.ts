6cf7daff39376891e6577920ca2bb8ebb27b5289
Preston Lamb <preston.j.lamb@gmail.com>
2021-01-24 23:07:47 -0700

    make new admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }];
+const routes: Routes = [
+	{ path: 'blog', loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule) },
+	{ path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule) },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
