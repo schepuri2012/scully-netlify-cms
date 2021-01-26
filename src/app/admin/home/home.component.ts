f1f5bfd3f92cbbc304c839bcfa1ba063f66b2b5b
Preston Lamb <preston.j.lamb@gmail.com>
2021-01-26 00:38:29 -0700

    inject the script during scully generate since Angular is disabled

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -14,12 +14,10 @@ export class HomeComponent implements OnInit {
 	}
 
 	ngOnInit(): void {
-		if (!isScullyRunning()) {
-			const script: HTMLScriptElement = this.renderer2.createElement('script');
-			script.type = 'text/javascript';
-			script.src = `https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js`;
-			script.text = '';
-			this.renderer2.appendChild(this._document.body, script);
-		}
+		const script: HTMLScriptElement = this.renderer2.createElement('script');
+		script.type = 'text/javascript';
+		script.src = `https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js`;
+		script.text = '';
+		this.renderer2.appendChild(this._document.body, script);
 	}
 }
