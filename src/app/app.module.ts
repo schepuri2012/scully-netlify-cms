b7f6757bd52594469db506a7d3c0ae278087313d
Preston Lamb <preston.j.lamb@gmail.com>
2021-01-26 13:55:49 -0700

    try using app base href token instead of in the index.html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ScullyLibModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, ScullyLibModule],
+	providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
