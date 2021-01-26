d08e19fcf9e4fdfc9238a197f07a575c8faf906f
Preston Lamb <preston.j.lamb@gmail.com>
2021-01-26 00:35:56 -0700

    trying with disabling Angular

--- scully.scully-netlify-cms.config.ts
+++ scully.scully-netlify-cms.config.ts
@@ -1,14 +1,16 @@
 import { ScullyConfig } from '@scullyio/scully';
+import { DisableAngular } from 'scully-plugin-disable-angular';
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "scully-netlify-cms",
-  outDir: './dist/static',
-  routes: {
-    '/blog/:slug': {
-      type: 'contentFolder',
-      slug: {
-        folder: "./blog"
-      }
-    },
-  }
+	projectRoot: './src',
+	projectName: 'scully-netlify-cms',
+	outDir: './dist/static',
+	routes: {
+		'/blog/:slug': {
+			type: 'contentFolder',
+			slug: {
+				folder: './blog',
+			},
+		},
+	},
+	defaultPostRenderers: [DisableAngular],
 };
