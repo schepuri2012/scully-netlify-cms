e7e674979a42d90319d087b9bfc57d3cfc7cc6f6
Preston Lamb <preston.j.lamb@gmail.com>
2021-01-26 23:00:46 -0700

    added blog list page

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -1,22 +1,26 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import {BlogComponent} from './blog.component';
+import { BlogComponent } from './blog.component';
+import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
-  {
-    path: ':slug',
-    component: BlogComponent,
-  },
-  {
-    path: '**',
-    component: BlogComponent,
-  }
+	{
+		path: '',
+		component: ListComponent,
+	},
+	{
+		path: ':slug',
+		component: BlogComponent,
+	},
+	{
+		path: '**',
+		component: ListComponent,
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class BlogRoutingModule {}
-
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,9 +3,10 @@ import {NgModule} from '@angular/core';
 import {ScullyLibModule} from '@scullyio/ng-lib';
 import {BlogRoutingModule} from './blog-routing.module';
 import {BlogComponent} from './blog.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [BlogComponent],
+  declarations: [BlogComponent, ListComponent],
   imports: [CommonModule, BlogRoutingModule, ScullyLibModule],
 })
 export class BlogModule {}
